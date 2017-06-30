cd0526e076a93b78a4d1143b4579ddfb464995e3
Rafael Vasquez <ravb2013@gmail.com>
2017-06-30 11:49:07 -0400

    Agregando modelos para perfiles de usuario y facultad con la migracion hecha

--- mysite/website/admin.py
+++ mysite/website/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Facultad, Perfil
 # Register your models here.
+
+admin.site.register(Facultad)
+admin.site.register(Perfil)
--- mysite/website/models.py
+++ mysite/website/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 # Create your models here.
+
+#Modelo para representar una facultad
+
+class Facultad(models.Model):
+	nombre = models.TextField(max_length=50);
+
+	def __str__(self):
+		return self.nombre
+
+# Modelo para el perfil del usuario y otros atributos.
+
+class Perfil(models.Model):
+	user = models.OneToOneField(User, on_delete = models.CASCADE)
+	desc = models.TextField(max_length=500,default='' ,blank=True)
+	facultad = models.ForeignKey(Facultad)
+	es_supervisor = models.BooleanField(default=False)
+	
+	def __str__(self):
+		return 'Perfil del usuario: %s' % self.user.username
+
+#esta seccion de codigo nos permite crear un modelo perfil
+#por cada usuario creado en el sistema automaticamente.
+
+def crear_perfil(sender, **kwargs):
+    user = kwargs["instance"]
+    if kwargs["created"]:
+        user_profile = Perfil(user=user)
+        user_profile.save()
+
+post_save.connect(crear_perfil, sender=User)
